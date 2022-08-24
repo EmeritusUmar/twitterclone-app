16a7c6d9927d5bd8e00c769b6afd20f7fe1d3bbb
Umar Musa <oladele7532@gmail.com>
2022-08-24 22:20:18 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+/*eslint-disable*/
+import React from "react";
+import "./App.css";
+import Main from "./components/main/main";
+import NavBar from "./components/navBar/navbar";
+import Right from "./components/right/right";
+
+const App = () => {
+  return (
+    <div id="container">
+      <div id="nav-box">
+        <NavBar />
+      </div>
+      <Main />
+      <Right />
+    </div>
+  );
+};
+
+export default App;
